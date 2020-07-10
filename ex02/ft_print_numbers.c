bc6b54f425d7926d5a0ddf0639bd5082fb7b901a
Eltonio Minnaar <eminnaar@c3r2s1.wethinkcode.co.za>
2020-07-10 11:03:38 +0200

    Completed ex02

---
+++ ex02/ft_print_numbers.c
@@ -0,0 +1,27 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eminnaar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/09 12:27:19 by eminnaar          #+#    #+#             */
+/*   Updated: 2020/07/09 12:37:21 by eminnaar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_numbers(void);
+
+void	ft_print_numbers(void)
+{
+	char	number;
+
+	number = '0';
+	while (number <= '9')
+		ft_putchar(number++);
+
+}
+
